694e4f53398f80e8a989dff63c0dc22dbb0f3ee7
VenomSnake96 <claudiojaimez.sec3@gmail.com>
2025-02-10 12:38:24 -0600

    Correction: all inpunt and output were changed to signals

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { booleanAttribute, Component, EventEmitter, input, Input, numberAttribute, Output } from "@angular/core";
+import { booleanAttribute, Component, EventEmitter, input, Input, numberAttribute, output, Output } from "@angular/core";
 
 function toCapitalize(text: string): string {
 if (!text) return '';
@@ -10,16 +10,27 @@ return text.charAt(0).toUpperCase() + text.slice(1);
   selector: 'app-child',
   standalone: true,
   imports:[],
-  template:`<p>Hello {{name}} and his salary {{salary}}</p>
-  <p><strong>Admin confirmation: {{isAdmin}}</strong></p>
+  template:`<p>Hello {{name()}} and his salary {{salary()}}</p>
+  <p><strong>Admin confirmation: {{isAdmin()}}</strong></p>
   <button (click)="sendMessage()">Send message</button>`,
 })
 export class Childcomponent {
-@Input({required:true, alias: 'title', transform:toCapitalize}) name!:string;
-@Input({transform: numberAttribute}) salary!: number
-@Input({transform: booleanAttribute}) isAdmin = false
+  name = input.required<string, string>({
+    alias: 'title',
+    transform: toCapitalize,
+  });
 
-@Output() messageEvent= new EventEmitter<string>()
+  salary = input<number, number>(0, {
+    transform: numberAttribute,
+  })
+
+  isAdmin = input<boolean, boolean>(false, {
+    transform: booleanAttribute,
+  })
+
+
+  messageEvent = output<string>()
+
 sendMessage(){
   this.messageEvent.emit('Hello Motherfucker')
 }
